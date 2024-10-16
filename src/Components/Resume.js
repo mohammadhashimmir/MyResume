c485fefb086930329c7455a1b4bae471fff10673
mohammadhashimmir <mohammadhashim619@gmail.com>
2024-10-16 15:34:56 +0530

    resume update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Resume.css";
+import pdf from "../Components/Resume 2024.pdf"
 
 const Resume = () => {
   return (
     <div className="resume-container">
       <iframe
         title="PDF"
-        src="https://firebasestorage.googleapis.com/v0/b/resume-app-16c2e.appspot.com/o/resume%202024.pdf?alt=media&token=b45a47a9-7eac-4df8-87d7-b2eaefc12b6d"
+        src={pdf}
         className="resume-iframe"
       ></iframe>
     </div>
